611f648ed3e21cf6b25dd9b206963e444651a24c
gbfelgas <gbfelgas@gmail.com>
2021-10-23 19:17:49 -0300

    fix: rename classes after selection and preprocess as a function

--- src/data.py
+++ src/data.py
@@ -6,79 +6,75 @@ import pandas as pd
 import torch
 from torch.utils.data import Dataset
 
-class EmbeddingsDataset(Dataset):
 
-    def __init__(
-        self,
-        embeddings_path: str = 'data/datasets/g1-news/articles_embeddings.pickle',
-        metadata_path: str = 'data/datasets/g1-news/articles_metadata.csv',
-        entry_column: str = "article_id",
-        class_column: str = "category_id",
-        class_threshold: float = 0.01
-    ):
-        super().__init__()
+def g1_news_preprocess(
+    embeddings_path: str,
+    metadata_path: str,
+    entry_column: str,
+    class_column: str,
+    class_threshold: float
+):
+    """
+    Cast data types to tensors and selects classes that
+    have more than <class_threshold> percent of dataset samples.
 
-        embeddings = pd.read_pickle(embeddings_path)
-        metadata = pd.read_csv(metadata_path)
-        self.X, self.y = self.preprocess(
-            embeddings,
-            metadata,
-            entry_column,
-            class_column,
-            class_threshold
+    Args:
+        embeddings_path (str):
+            Input embeddings dataset path, it contains the embeddings of all entries.
+            (size: (number of entries, embeddings dimension))
+        metadata_path (str):
+            Metadata dataset path, it contains metadata of all entries
+            and assumes classes are storage in <class_column>.
+            (size: (number of entries, ...))
+        entry_column (str):
+            Column of metadata dataset where entry id is stored.
+        class_column (str):
+            Column of metadata dataset where class id is stored.
+        class_threshold (float):
+            Thrshold to select classes and samples. Only classes with more than
+            total number of samples * class_threshold will be selected.
+
+    Returns:
+        X (np.ndarray):
+            Filtered input dataset, with only selected classes entries.
+            (size: (number of entries from the selected classes, embeddings dimension))
+        y (np.ndarray):
+            Filtered output dataset, with only selected classes entries.
+            (size: (number of entries from the selected classes,))
+    """
+    
+    embeddings = pd.read_pickle(embeddings_path)
+    metadata = pd.read_csv(metadata_path)
+
+    emb_size = len(embeddings)
+    met_size = len(metadata)
+
+    if emb_size != met_size:
+        raise ValueError(
+            f"Embeddings and metadata must have the same number of entries!\n\
+                Number of embeddings entries = {emb_size}\n\
+                Number of metadata entries = {met_size}"
         )
 
-    @staticmethod
-    def preprocess(
-        embeddings: np.ndarray,
-        metadata: pd.DataFrame,
-        entry_column: str,
-        class_column: str,
-        class_threshold: float
-    ):
-        """
-        Cast data types to tensors and selects classes that
-        have more than <class_threshold> percent of dataset samples.
+    grouped_metadata = metadata.groupby(by=[class_column]).count()
+    selected_classes = grouped_metadata[grouped_metadata[entry_column] >= (class_threshold * met_size)].index
+    n_classes = len(selected_classes)
+    map_classes = {selected_classes[i]: i for i in range(n_classes)}
 
-        Args:
-            embeddings (np.ndarray):
-                Input embeddings dataset, it contains the embeddings of all entries.
-                (size: (number of entries, embeddings dimension))
-            metadata (pd.DataFrame):
-                Metadata dataset, it contains metadata of all entries
-                and assumes classes are storage in <class_column>.
-                (size: (number of entries, ...))
+    filtered_metadata = metadata[metadata[class_column].isin(selected_classes)].copy()
+    filtered_metadata[class_column] = filtered_metadata[class_column].apply(lambda x: map_classes[x])
+    X = embeddings[filtered_metadata[entry_column].values, :]
+    y = filtered_metadata[class_column].values
+    emb_dim = X.shape[-1]
 
-        Returns:
-            X (torch.tensor):
-                Filtered input dataset, with only selected classes entries.
-                (size: (number of entries from the selected classes, embeddings dimension))
-            y (torch.tensor):
-                Filtered output dataset, with only selected classes entries.
-                (size: (number of entries from the selected classes,))
-        """
+    return X, y, emb_dim, n_classes
+    
 
-        emb_size = len(embeddings)
-        met_size = len(metadata)
+class EmbeddingsDataset(Dataset):
 
-        if emb_size != met_size:
-            raise ValueError(
-                f"Embeddings and metadata must have the same number of entries!\n\
-                    Number of embeddings entries = {emb_size}\n\
-                    Number of metadata entries = {met_size}"
-            )
-
-        grouped_metadata = metadata.groupby(by=[class_column]).count()
-        selected_classes = grouped_metadata[grouped_metadata[entry_column] >= (class_threshold * met_size)].index
-        print(f"Total number of classes = {metadata[class_column].nunique()}")
-        print(f"Number of selected classes = {len(selected_classes)}")
-
-        filtered_metadata = metadata[metadata[class_column].isin(selected_classes)]
-
-        X = torch.tensor(embeddings[filtered_metadata[entry_column].values, :])
-        y = torch.tensor(filtered_metadata[class_column].values)
-
-        return X, y
+    def __init__(self, X: torch.tensor, y: torch.tensor):
+        super().__init__()
+        self.X, self.y = torch.tensor(X), torch.tensor(y)
 
     def __getitem__(self, idx):
         return self.X[idx], self.y[idx]
