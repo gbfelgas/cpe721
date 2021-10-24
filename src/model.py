fix: deduplicate final layer softmax, remove it from MLP

src/model.py
@@ -8,21 +8,19 @@ class MLP(nn.Module):
         hidden_dim: int,
         output_dim: int,
         dropout: float,
-        num_layers: int,
-        with_softmax: bool
+        num_layers: int
     ):
         super().__init__()
         self.layers = nn.ModuleList()
+        # self.layers.append(nn.BatchNorm1d(input_dim))
         for n in range(num_layers):
-            if n != (num_layers - 1):
+            if n < (num_layers - 1):
                 self.layers.append(nn.Linear(input_dim, hidden_dim))
+                # nn.init.kaiming_normal_(self.layers[-1].weight, mode="fan_in")
                 self.layers.append(nn.ReLU())
                 self.layers.append(nn.Dropout(dropout))
                 input_dim = hidden_dim
-            else:
-                self.layers.append(nn.Linear(input_dim, output_dim))
-                if with_softmax:
-                    self.layers.append(nn.Softmax(dim=-1))
+        self.layers.append(nn.Linear(input_dim, output_dim))
 
     def forward(self, x):
         for layer in self.layers:

src/module.py
@@ -12,7 +12,6 @@ class LightningModuleMLP(pl.LightningModule):
         output_dim: int,
         dropout: float = 0.0,
         num_layers: int = 2,
-        with_softmax: bool = True,
         learning_rate: float = 0.01
     ):
         super().__init__()
@@ -22,8 +21,7 @@ class LightningModuleMLP(pl.LightningModule):
             hidden_dim,
             output_dim,
             dropout,
-            num_layers,
-            with_softmax
+            num_layers
         )
 
         self.criterion = nn.CrossEntropyLoss()