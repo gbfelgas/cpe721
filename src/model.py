1358d27ac3630aefb2646a77fcad05f9a05653bc
gbfelgas <gbfelgas@gmail.com>
2021-10-31 00:52:50 -0300

    feat: add batch normalization and he initialization

--- src/model.py
+++ src/model.py
@@ -12,11 +12,11 @@ class MLP(nn.Module):
     ):
         super().__init__()
         self.layers = nn.ModuleList()
-        # self.layers.append(nn.BatchNorm1d(input_dim))
+        self.layers.append(nn.BatchNorm1d(input_dim))
         for n in range(num_layers):
             if n < (num_layers - 1):
                 self.layers.append(nn.Linear(input_dim, hidden_dim))
-                # nn.init.kaiming_normal_(self.layers[-1].weight, mode="fan_in")
+                nn.init.kaiming_normal_(self.layers[-1].weight, mode="fan_in")
                 self.layers.append(nn.ReLU())
                 self.layers.append(nn.Dropout(dropout))
                 input_dim = hidden_dim
